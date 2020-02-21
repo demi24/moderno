1fb76d0182bf60bfabc0a717d215322d06f74168
demi24 <appega@mail.ru>
2020-02-21 16:15:51 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,16 @@ $(function(){
     var mixer = mixitup('.products__inner-box');
 
   
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+      $(".product-slider-inner").slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+      });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,8 @@ gulp.task('css', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/rateyo/src/jquery.rateyo.css'
+
   ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -46,7 +48,8 @@ gulp.task('script', function(){
 gulp.task('js', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/mixitup/dist/mixitup.js'
+    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/rateyo/src/jquery.rateyo.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
