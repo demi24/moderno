f0125e2e677004373bdd74d5513a900c5f4f2de1
demi24 <appega@mail.ru>
2020-02-22 23:09:46 +0300

    added new-items.html elements

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,5 @@
 $(function(){
 
-    var mixer = mixitup('.products__inner-box');
-
-  
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
@@ -15,4 +12,16 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4
       });
+
+      $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    var mixer = mixitup('.products__inner-box');
+
 });
