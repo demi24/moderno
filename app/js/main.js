652ccccb4174afbc11a734124775884ebae2c0e7
demi24 <appega@mail.ru>
2020-02-28 11:32:27 +0300

    added button list js filter to new-items.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,38 @@
-$(function(){
+$(function () {
 
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly: true
-      });
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
 
-      $(".product-slider-inner").slick({
-        dots: true,
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 4
-      });
+  $(".product-slider-inner").slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
 
-      $(".js-range-slider").ionRangeSlider({
-        type: "double",
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 600,
-        prefix: "$"
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+
+  $('.icon-th-list').on('click', function () {
+    $('.product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active')
+  });
+  $('.icon-th-large').on('click', function(){
+    $('.product__item').removeClass('list');
+    $('.icon-th-list').removeClass('active');
+    $('.icon-th-large').addClass('active')
     });
 
-    var mixer = mixitup('.products__inner-box');
+  var mixer = mixitup('.products__inner-box');
 
 });
